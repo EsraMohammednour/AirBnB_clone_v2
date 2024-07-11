bfa9d2cee3800819d78af9e9a4d8166505ce0759
esraa <esraa2023112@gmail.com>
2024-07-11 16:08:21 +0300

    done

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Fabric script that generates a .tgz"""
-import fabric.api
+from fabric.api import local
 import datatime
 import os.path
 
@@ -8,10 +8,15 @@ import os.path
 def do_pack():
     """Archive from the contents of the web_static"""
     d = datatime.datatime.now()
-    esra = "versions/web_static_{}{}{}{}{}{}.tgz".format(d.year, d.month, d.day, d.hour, d.minute, d.second)
+    esra = "versions/web_static_{}{}{}{}{}{}.tgz".format(d.year,
+                                                            d.month,
+                                                            d.day,
+                                                            d.hour,
+                                                            d.minut,
+                                                            d.second)
     if os.path.isdir("versions") is False:
-        if fabric.api.local("mkdir -p versions").failed is True:
+        if local("mkdir -p versions").failed is True:
             return None
-    if fabric.api.local(f"tar -cvzf {} web_static".format(esra)).failed is True:
+    if local(f"tar -cvzf {} web_static".format(esra)).failed is True:
         return None
     return esra
