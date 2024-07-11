b4951e61020c8dfa830bad71f8f7328926406c8c
esraa <esraa2023112@gmail.com>
2024-07-11 16:00:55 +0300

    done

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,10 +8,10 @@ import os.path
 def do_pack():
     """Archive from the contents of the web_static"""
     d = datatime.datatime.now()
-    esra = f"versions/web_static_{d.year}{d.month}{d.day}{d.hour}{d.minute}{d.second}.tgz"
+    esra = "versions/web_static_{}{}{}{}{}{}.tgz".format(d.year, d.month, d.day, d.hour, d.minute, d.second)
     if os.path.isdir("versions") is False:
         if fabric.api.local("mkdir -p versions").failed is True:
             return None
-    if fabric.api.local(f"tar -cvzf {esra} web_static").failed is True:
+    if fabric.api.local(f"tar -cvzf {} web_static".format(esra)).failed is True:
         return None
     return esra
