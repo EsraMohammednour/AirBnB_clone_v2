fc04183fb2a552b3d684d734b2d6da1d2f17fef1
esraa <esraa2023112@gmail.com>
2024-07-27 17:13:03 +0300

    done

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''Starts a Flask web application'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    ''' Return wellome massege'''
+    return "Hello HBNB!"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_is_fun(text):
+    ''' Return the massege'''
+    no_underscore = text.replace('_', ' ')
+    return f"C {no_underscore}"
+
+
+@app.route('/python', defaults={'text': 'is_cool'})
+@app.route('/python/<text>')
+def python_is_fun(text):
+    ''' Return the massege'''
+    text2 = text.replace('_', ' ')
+    return f"python {text2}"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    ''' Return wellome massege'''
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
