6f6be6e33609ffd52e90d6b9958380218f977713
esraa <esraa2023112@gmail.com>
2024-07-27 16:07:21 +0300

    done

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Starts a Flask web application'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    ''' Return wellome massege'''
+    return "Hello HBNB!"
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_is_fun(text):
+    ''' Return the massege'''
+    no_underscore =  text.replace('_', ' ')
+    return f"C {no_underscore}"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    ''' Return wellome massege'''
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
