fae08b4814f4354333b1d64e0e83215237ed9e7c
esraa <esraa2023112@gmail.com>
2024-07-27 19:10:43 +0300

    done

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+'''Starts a Flask web application'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    ''' Return wellome massege'''
+    return "Hello HBNB!"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_is_fun(text):
+    ''' Return the massege'''
+    no_underscore = text.replace('_', ' ')
+    return f"C {no_underscore}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """Return the massege"""
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Return the massege"""
+    return f"{n} is a number"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    ''' Return wellome massege'''
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
