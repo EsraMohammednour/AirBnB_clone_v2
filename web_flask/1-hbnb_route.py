7f75466ea20591a622fd43fd640859242c7f9250
esraa <esraa2023112@gmail.com>
2024-07-27 15:36:26 +0300

    done

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,5 +10,11 @@ def Hello_HBNB():
     return "Hello HBNB!"
 
 
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    ''' Return wellome massege'''
+    return "HBNB"
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
